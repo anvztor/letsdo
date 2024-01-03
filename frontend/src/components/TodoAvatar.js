44d740cb3ec4ea2e72b49e0353c87564712f56b4
justor95 <justor95@hotmail.com>
2024-01-04 02:03:15 +0800

    impl: profileAvatar support accomplish icon & color change

--- frontend/src/components/TodoAvatar.js
+++ frontend/src/components/TodoAvatar.js
@@ -1,51 +1,66 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-import CircularProgress from '@mui/material/CircularProgress';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
+import { green } from '@mui/material/colors';
+import Fab from '@mui/material/Fab';
+import CheckIcon from '@mui/icons-material/Check';
+import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 
-function TodoAvatar(props) {
+export default function CircularDeterminate({id, title, body}) {
+  const [progress, setProgress] = React.useState(0);
+  const [success, setSuccess] = React.useState(0);
+  
+  const buttonSx = {
+    ...(success && {
+      bgcolor: green[500],
+      '&:hover': {
+        bgcolor: green[700],
+      },
+    }),
+  };
+  const quest = JSON.parse(title)
+  const completeness = parseInt(quest.args[0].score) / parseInt(quest.args[0].target) * 100
+  React.useEffect(() => {
+    const timer = setInterval(() => {
+      if (completeness >= 100) {
+        setSuccess(true);
+        setProgress(0);
+      } else {
+        setProgress(completeness);
+      }
+    }, 3000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+  
   return (
-    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-      <CircularProgress variant="determinate" {...props} />
-      <Box
-        sx={{
-          top: 0,
-          left: 0,
-          bottom: 0,
-          right: 0,
-          position: 'absolute',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
-        <Typography variant="caption" component="div" color="text.secondary">
-          {`${Math.round(props.id)}`}
-        </Typography>
+    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+      <Box sx={{ m: 1, position: 'relative' }}>
+        <Fab
+          aria-label="save"
+          color="primary"
+          sx={buttonSx}
+        >
+          {success ? <CheckIcon /> : <CatchingPokemonIcon />}
+        </Fab>
+        
+          <CircularProgress
+            size={68}
+            variant="determinate"
+            value={progress}
+            sx={{
+              color: green[500],
+              position: 'absolute',
+              top: -6,
+              left: -6,
+              zIndex: 1,
+            }}
+          />
+        
       </Box>
+      
     </Box>
   );
-}
-
-TodoAvatar.propTypes = {
-  /**
-   * The value of the progress indicator for the determinate variant.
-   * Value between 0 and 100.
-   * @default 0
-   */
-  value: PropTypes.number.isRequired,
-};
-
-export default function CircularWithValueLabel({id, title, body}) {
-  const [progress, setProgress] = React.useState(10);
-  
-  React.useEffect(() => {
-    const quest = JSON.parse(title)
-    const completeness = parseInt(quest.args[0].score) / parseInt(quest.args[0].target) * 100
-    const completerate = (completeness > 100) ? 100 : completeness
-    setProgress(completerate);
-  }, []);
-
-  return <TodoAvatar id={id} title={title} body={body} value={progress} />;
 }
